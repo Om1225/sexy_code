b39ca9c70494db98351f3c4360de079b719bb707
OmRay <omray02@gmail.com>
2023-10-12 23:23:55 +0530

    sexy_code-1

---
+++ Code10.cpp
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+
+// int main() {
+//     int n, i;
+//     printf("Enter the number of terms: ");
+//     scanf("%d", &n);
+//     printf("Square Series: ");
+//     for (i = 1; i <= n; i++) {
+//         printf("%d ", i * i);
+//     }
+//     return 0;
+// }
+
+
+# include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello";
+    return 0;
+}
